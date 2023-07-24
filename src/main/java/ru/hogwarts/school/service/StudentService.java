111696ba7c2ba15c4bf83eaf50c314dd54cc9d97
SergeyParadigm77 <paradigm77@rambler.ru>
2023-07-24 23:47:31 +0300

    ДЗ выполнено!

--- src/main/java/ru/hogwarts/school/service/StudentService.java
+++ src/main/java/ru/hogwarts/school/service/StudentService.java
@@ -1,5 +1,7 @@
 package ru.hogwarts.school.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import ru.hogwarts.school.model.Faculty;
 import ru.hogwarts.school.model.Student;
@@ -16,44 +18,59 @@ public class StudentService {
     public StudentService(StudentRepository studentRepository) {
         this.studentRepository = studentRepository;
     }
+    private final static Logger logger = LoggerFactory.getLogger(StudentService.class);
 
 
     public Student createStudent (Student student) {
+        logger.info("createStudent method was invoked");
         return studentRepository.save(student);
     }
-    public Student findStudentById(Long id) {
+    public Student findStudentById(Long id)
+    {
+        logger.info("findStudentById method was invoked");
         return studentRepository.findById(id).orElse(null);
     }
     public Student editStudent(Student student) {
+        logger.info("editStudent method was invoked");
         return studentRepository.save(student);
     }
     public void deleteStudent(Long id) {
+        logger.info("deleteStudent method was invoked");
         studentRepository.deleteById(id);
     }
     public Collection<Student> getAll() {
+        logger.info("getAll method was invoked");
         return studentRepository.findAll();
     }
     public Collection<Student> getAllByAge(int age) {
+        logger.info("getAllByAge method was invoked");
         return getAll().stream().filter(it -> it.getAge() == age).collect(Collectors.toList());
     }
     public ArrayList<Student> findByAgeBetweenOrderByAge(int min, int max) {
+        logger.info("findByAgeBetweenOrderByAge method was invoked");
         return studentRepository.findByAgeBetweenOrderByAge(min, max);
     }
 
     public Faculty getFacultyByStudentId(Long id) {
+        logger.info("getFacultyByStudentId method was invoked");
         return studentRepository.findById(id).map(Student::getFaculty).orElse(null);
     }
-    public Collection<Student> findAllByFacultyId(Long id) {
+    public Collection<Student> findAllByFacultyId(Long id)
+    {
+        logger.info("findAllByFacultyId method was invoked");
         return studentRepository.findAllByFacultyId(id);
     }
 
     public int getCountOfStudents() {
+        logger.info("getCountOfStudents method was invoked");
         return studentRepository.getCountOfStudents();
     }
     public int getAverageAge() {
+        logger.info("getAverageAge method was invoked");
         return studentRepository.getAverageAge();
     }
     public List<Student> getFiveStudentsOrderedById() {
+        logger.info("getFiveStudentsOrderedById method was invoked");
         return studentRepository.getFiveStudentsOrderedById();
     }
     }
